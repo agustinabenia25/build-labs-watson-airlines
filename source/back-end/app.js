d6808bf7bd494211dff0e97b4efed8170495e7c6
agustinabenia25 <agustina.benia@correo.ucu.edu.uy>
2023-08-21 18:41:44 +0100

    minor changes

--- source/back-end/app.js
+++ source/back-end/app.js
@@ -31,6 +31,7 @@ async function main(){
         res.status(200).send({
             message: "Welcome to Watson Assistant!!"
         })
+        console.log("Root URL request.");
     });
 
     // GET route set up for "About Watson Airlines"
@@ -38,6 +39,7 @@ async function main(){
         res.status(200).send({
             message: "Watson Airlines is a one of the largest airlines in America. With over 30 years of history, we connect people to opportunities while expanding the understanding of our planet and the people within it. We offer our one-of-a-kind value and Hospitality at over 50 airports across more than 15 countries. In addition, we are members of the International Air Transport Association (IATA), a trade association that represents over 300 airlines, equivalent to about 83% of total air traffic. This allows us to operate safely, securely, efficiently, and economically under clearly defined rules. We are pioneers in the usage of technology, and actively advocate for its usage to improve our customer's experience."
         })
+        console.log("About Watson Airlines request.");
     });
 
     // Connecting to MongoDB database
